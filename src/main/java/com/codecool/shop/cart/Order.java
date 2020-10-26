7c7939aa19aacbfa1a1fae5885c9b8f9a93d3322
StolarzPrzemyslaw <stolarz.przemyslaw@gmail.com>
2020-10-26 21:21:59 +0100

    Implement Order

---
+++ src/main/java/com/codecool/shop/cart/Order.java
@@ -0,0 +1,49 @@
+package com.codecool.shop.cart;
+
+import com.codecool.shop.model.Product;
+
+public class Order {
+    private final int id;
+    private final Product lineItem;
+    private int quantity;
+    private float totalPrice;
+
+    public Order(Product lineItem, int id) {
+        this.id = id;
+        this.lineItem = lineItem;
+        this.quantity = 1;
+        this.totalPrice = lineItem.getDefaultPrice() * quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public float getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice() {
+        this.totalPrice = lineItem.getDefaultPrice() * quantity;
+    }
+
+    public Product getLineItem() {
+        return lineItem;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void increaseQuantity() {
+        quantity++;
+    }
+
+    public void decreaseQuantity() {
+        quantity--;
+    }
+}
