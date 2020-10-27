9f0a126c9cf04e820f8988d6832837ba0cf1116d
DanielRzeszutko <daniel.rzeszutko0@gmail.com>
2020-10-27 14:50:04 +0100

    Filters by supplier and by category works separately

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -5,7 +5,8 @@ import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.config.TemplateEngineUtil;
-import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.model.Product;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -15,36 +16,43 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 @WebServlet(urlPatterns = {"/"})
 public class ProductController extends HttpServlet {
+    List<Product> products;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
-
+        SupplierDaoMem supplierDataStore = SupplierDaoMem.getInstance();
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
 
-        String categoryId = (req.getParameter("order"));
-        if (categoryId != null) {
-            int id = Integer.parseInt(categoryId);
-            context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(id)));
-        } else {
-            context.setVariable("products", productDataStore.getAll());
+        String category = (req.getParameter("category"));
+        String supplier = (req.getParameter("supplier"));
+
+        if (category != null && supplier == null) {
+            products = productDataStore.getBy(productCategoryDataStore.getCategoryByName(category));
+        }
+        else if (category == null && supplier != null) {
+            products = productDataStore.getBy(supplierDataStore.getSipplierByName(supplier));
+        }
+        else {
+            products = productDataStore.getAll();
         }
 
+        context.setVariable("products", products);
         context.setVariable("categories", productCategoryDataStore.getAll());
-
-        // // Alternative setting of the template context
-        // Map<String, Object> params = new HashMap<>();
-        // params.put("category", productCategoryDataStore.find(1));
-        // params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
-        // context.setVariables(params);
+        context.setVariable("suppliers", supplierDataStore.getAll());
         engine.process("product/index.html", context, resp.getWriter());
     }
-
 }
+
+
+// // Alternative setting of the template context
+// Map<String, Object> params = new HashMap<>();
+// params.put("category", productCategoryDataStore.find(1));
+// params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
+// context.setVariables(params);
