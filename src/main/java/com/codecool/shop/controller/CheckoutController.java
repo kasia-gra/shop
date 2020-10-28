380188b5fe7deb7f1e9b6aa403d39478b2a8eab2
StolarzPrzemyslaw <stolarz.przemyslaw@gmail.com>
2020-10-28 21:47:45 +0100

    Implement cart displaying in checkout.html

--- src/main/java/com/codecool/shop/controller/CheckoutController.java
+++ src/main/java/com/codecool/shop/controller/CheckoutController.java
@@ -1,6 +1,10 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.OrderDao;
+import com.codecool.shop.dao.implementation.OrderDaoMem;
+import com.codecool.shop.model.order.Order;
+import com.codecool.shop.model.product.Product;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -10,6 +14,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet(urlPatterns = {"/checkout"}, loadOnStartup = 3)
 public class CheckoutController extends HttpServlet {
@@ -18,10 +24,27 @@ public class CheckoutController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
+        Map<Product, Integer> orderedProducts = new HashMap<>();
+        float totalPrice = 0;
+        String cartValue = "0";
+        int itemsNumber = 0;
+        String currency = "";
 
-//        context.setVariable("itemsNumber", itemsNumber);
-//        context.setVariable("orderedProducts", orderedProducts);
-//        context.setVariable("cartValue", cartValue);
+        if (CartController.getCookieValueBy("userId", req) != null) {
+            OrderDao orderDataStore = OrderDaoMem.getInstance();
+            Order order = orderDataStore.getActual(Integer.parseInt(CartController.getCookieValueBy("userId", req)));
+            orderedProducts = order.getCart().getProducts();
+            totalPrice = order.getCart().getTotalPrice();
+            HashMap.Entry<Product, Integer> entry = orderedProducts.entrySet().iterator().next();
+            currency = entry.getKey().getDefaultCurrency().getCurrencyCode();
+            cartValue = String.format("%.2f", totalPrice);
+            itemsNumber = order.getCart().getSize();
+        }
+
+        context.setVariable("currency", currency);
+        context.setVariable("itemsNumber", itemsNumber);
+        context.setVariable("orderedProducts", orderedProducts);
+        context.setVariable("cartValue", cartValue);
         engine.process("product/checkout.html", context, resp.getWriter());
     }
 
