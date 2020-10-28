2099048000b02155401cac7e0251b84493dfc96d
StolarzPrzemyslaw <stolarz.przemyslaw@gmail.com>
2020-10-28 21:10:54 +0100

    Implement CheckoutController and checkoutManager

---
+++ src/main/webapp/static/js/checkoutManager.js
@@ -0,0 +1,43 @@
+let checkoutManager = {
+  init: function() {
+    checkoutManager.initValidation();
+    checkoutManager.toggleShippingAddressVisibility();
+  },
+  initValidation: function() {
+    window.addEventListener('load', function () {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      let forms = document.querySelectorAll(".needs-validation:not(.hidden)")
+      // Loop over them and prevent submission
+      Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener('submit', function (event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+
+        }, false);
+      })
+    }, false);
+    let submitButton = document.querySelector("#submitButton");
+    // submitButton.addEventListener("submit", () => {
+    //   document.querySelector("#billingAddressForm").submit(event => {
+    //     event.preventDefault();
+    //     event.stopPropagation()
+    //     alert("submitted")
+    //     console.log("submitted");
+    //   });
+  },
+  toggleShippingAddressVisibility: function() {
+    let sameAddressCheckbox = document.querySelector("#same-address");
+    sameAddressCheckbox.addEventListener("change", checkoutManager.toggleShippingAddressVisibilityHandler);
+  },
+  toggleShippingAddressVisibilityHandler: function() {
+    let shippingAddressContainer = document.querySelector("#shippingAddressContainer");
+    shippingAddressContainer.classList.toggle("hidden");
+    shippingAddressContainer.disabled = !shippingAddressContainer.disabled;
+
+  }
+}
+
+checkoutManager.init();
