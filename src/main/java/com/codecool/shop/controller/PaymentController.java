4ff6216159a06ab8413268b1637eefd8999b291e
StolarzPrzemyslaw <stolarz.przemyslaw@gmail.com>
2020-10-29 16:08:01 +0100

    Implement saving json to .json file

--- src/main/java/com/codecool/shop/controller/PaymentController.java
+++ src/main/java/com/codecool/shop/controller/PaymentController.java
@@ -4,6 +4,7 @@ import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.dao.dao.OrderDao;
 import com.codecool.shop.dao.jdbc.OrderDaoMem;
 import com.codecool.shop.model.order.Order;
+import com.google.gson.Gson;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -12,13 +13,14 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.io.*;
 import java.util.Objects;
 
-@WebServlet(urlPatterns = {"/payment"}, loadOnStartup = 3)
+@WebServlet(urlPatterns = {"/payment"}, loadOnStartup = 4)
 public class PaymentController extends HttpServlet {
 
 	Util util = new Util();
+	Gson gson = new Gson();
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -46,6 +48,26 @@ public class PaymentController extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+		//TODO confirm payment
+		OrderDao orderDataStore = OrderDaoMem.getInstance();
+		Order order = orderDataStore.getActual(Integer.parseInt(Objects.requireNonNull(util.getCookieValueBy("userId", req))));
+		saveOrderToFile(order);
+		resp.sendRedirect("/paymentConfirmation");
 	}
+
+	private void saveOrderToFile(Order order) throws IOException {
+		String jsonOrder = gson.toJson(order);
+		String relativeWebPath = "/orders";
+		String absoluteDiskPath = getServletContext().getRealPath(relativeWebPath);
+		String filename = "order" + order.getId() + ".json";
+		File file = new File(absoluteDiskPath, filename);
+		saveFileToDisk(jsonOrder, file);
+	}
+
+	private void saveFileToDisk(String jsonOrder, File file) throws IOException {
+		FileWriter fileWriter = new FileWriter(file);
+		fileWriter.write(jsonOrder);
+		fileWriter.flush();
+	}
+
 }
