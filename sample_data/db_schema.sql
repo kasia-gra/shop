5cf2c340cc8b95195c9383d59ba5bccc89099a8a
DanielRzeszutko <daniel.rzeszutko0@gmail.com>
2020-11-09 15:28:36 +0100

    Add sql script to create DB schema

--- sample_data/db_schema.sql
+++ sample_data/db_schema.sql
@@ -1,47 +1,146 @@
-DROP TABLE IF EXISTS public.products;
-CREATE TABLE public.products (
-    id serial NOT NULL PRIMARY KEY,
-    name text NOT NULL,
-    price float NOT NULL,
-    currency text NOT NULL,
-    description text NOT NULL,
-    picture_name text NOT NULL,
-    category_id int NOT NULL,
-    supplier_id int NOT NULL
+DROP TYPE IF EXISTS status CASCADE;
+CREATE TYPE status AS ENUM ('paid', 'checked', 'confirmed', 'shipped');
+
+
+DROP TABLE IF EXISTS public.order;
+CREATE TABLE public.order
+(
+    id                       serial NOT NULL PRIMARY KEY,
+    cart_id                  int    NOT NULL,
+    user_id                  int    NOT NULL,
+    order_address_details_id int    NOT NULL,
+    date                     timestamp,
+    current_status           status
+
 );
 
-DROP TABLE IF EXISTS public.categories;
-CREATE TABLE public.categories (
-    id serial NOT NULL PRIMARY KEY,
-    name text NOT NULL,
-    department text NOT NULL,
-    description text NOT NULL
+
+DROP TABLE IF EXISTS public.cart CASCADE;
+CREATE TABLE public.cart
+(
+    id          serial NOT NULL PRIMARY KEY,
+    total_price int    NOT NULL,
+    cart_size   int    NOT NULL
 );
 
-DROP TABLE IF EXISTS public.suppliers;
-CREATE TABLE public.suppliers (
-    id serial NOT NULL PRIMARY KEY,
-    name text NOT NULL,
-    description text NOT NULL
+
+DROP TABLE IF EXISTS public.line_item;
+CREATE TABLE public.line_item
+(
+    id               serial NOT NULL PRIMARY KEY,
+    cart_id          int    NOT NULL,
+    product_id       int    NOT NULL,
+    quantity         int    NOT NULL,
+    total_line_price int    NOT NULL
 );
 
-DROP TABLE IF EXISTS public.users;
-CREATE TABLE public.users (
-    id serial NOT NULL PRIMARY KEY,
-    login text NOT NULL,
-    email text NOT NULL,
-    password text NOT NULL,
-    phone_number text,
-    registration_date timestamp,
-    billing_address_id int NOT NULL,
-    address_id int NOT NULL
+
+DROP TABLE IF EXISTS public.product;
+CREATE TABLE public.product
+(
+    id            serial NOT NULL PRIMARY KEY,
+    name          text   NOT NULL,
+    default_price float  NOT NULL,
+    description   text   NOT NULL,
+    picture_name  text   NOT NULL,
+    category_id   int    NOT NULL,
+    supplier_id   int    NOT NULL
 );
+
+
+DROP TABLE IF EXISTS public.category;
+CREATE TABLE public.category
+(
+    id          serial NOT NULL PRIMARY KEY,
+    name        text   NOT NULL,
+    department  text   NOT NULL,
+    description text   NOT NULL
+);
+
+
+DROP TABLE IF EXISTS public.supplier;
+CREATE TABLE public.supplier
+(
+    id          serial NOT NULL PRIMARY KEY,
+    name        text   NOT NULL,
+    description text   NOT NULL
+);
+
+
+DROP TABLE IF EXISTS public.user;
+CREATE TABLE public.user
+(
+    id                  serial NOT NULL PRIMARY KEY,
+    first_name          text   NOT NULL,
+    last_name           text   NOT NULL,
+    email               text   NOT NULL,
+    password            text   NOT NULL,
+    phone_number        text,
+    registration_date   timestamp,
+    billing_address_id  int    NOT NULL,
+    shipping_address_id int    NOT NULL
+);
+
+
+DROP TABLE IF EXISTS public.order_address_detail;
+CREATE TABLE public.order_address_detail
+(
+    id                        serial NOT NULL PRIMARY KEY,
+    order_billing_address_id  int    NOT NULL,
+    order_shipping_address_id int    NOT NULL
+);
+
 
 DROP TABLE IF EXISTS public.address;
-CREATE TABLE public.address (
-    id serial NOT NULL PRIMARY KEY,
-    country text NOT NULL,
-    city text NOT NULL,
-    zip_code text NOT NULL,
-    address text NOT NULL
+CREATE TABLE public.address
+(
+    id       serial NOT NULL PRIMARY KEY,
+    country  text   NOT NULL,
+    city     text   NOT NULL,
+    zip_code text   NOT NULL,
+    address  text   NOT NULL
 );
+
+ALTER TABLE ONLY "order"
+    ADD CONSTRAINT "cart_id" FOREIGN KEY (cart_id)
+        REFERENCES cart (id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY "order"
+    ADD CONSTRAINT "user_id" FOREIGN KEY (user_id)
+        REFERENCES "user" (id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY "order"
+    ADD CONSTRAINT "order_address_detail" FOREIGN KEY (order_address_details_id)
+        REFERENCES "order_address_detail" (id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY "line_item"
+    ADD CONSTRAINT "cart_id" FOREIGN KEY (cart_id)
+        REFERENCES "cart" (id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY "line_item"
+    ADD CONSTRAINT "product_id" FOREIGN KEY (product_id)
+        REFERENCES "product" (id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY "product"
+    ADD CONSTRAINT "category_id" FOREIGN KEY (category_id)
+        REFERENCES "category" (id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY "product"
+    ADD CONSTRAINT "supplier_id" FOREIGN KEY (supplier_id)
+        REFERENCES "supplier" (id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY "order_address_detail"
+    ADD CONSTRAINT "order_address_detail_id" FOREIGN KEY (order_billing_address_id)
+        REFERENCES "address" (id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY "order_address_detail"
+    ADD CONSTRAINT "order_shipping_address_id" FOREIGN KEY (order_shipping_address_id)
+        REFERENCES "address" (id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY "user"
+    ADD CONSTRAINT "billing_address_id" FOREIGN KEY (billing_address_id)
+        REFERENCES "address" (id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY "user"
+    ADD CONSTRAINT "shipping_address_id" FOREIGN KEY (shipping_address_id)
+        REFERENCES "address" (id) ON DELETE CASCADE;
